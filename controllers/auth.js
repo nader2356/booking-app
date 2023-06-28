9528039134f54cf4e9f1a22c0740e4f29ca09511
nader2356 <naderabdessaied8@gmail.com>
2023-06-29 00:41:19 +0200

    testtoken of user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-import User from "../models/User.js";
+import User from "../models/Users.js";
 import bcrypt from "bcryptjs";
 import { createError } from "../utils/error.js";
 import jwt from "jsonwebtoken";
--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
 
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 
-app.listen(4000, () => {
-    mongoose.connect(`mongodb://127.0.0.1:27017/booking_app`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log(`🚀 application ready at 4000`);
+const connect = async () => {
+  try {
+    await mongoose.connect(`mongodb://127.0.0.1:27017/listeDesVoiture`);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+//middlewares
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
   });
+});
+
+app.listen(9800, () => {
+  connect();
+  console.log("Connected to backend.");
+});
